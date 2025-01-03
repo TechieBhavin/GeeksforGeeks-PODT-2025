24877612d65fe99e42af138e39e342d252ebe71c
Patel Bhavin <154894074+TechieBhavin@users.noreply.github.com>
2025-01-03 19:38:24 +0530

    Update solution.java

--- PODT-of-JANUARY/Day1-Print Anagrams Together/solution.java
+++ PODT-of-JANUARY/Day1-Print Anagrams Together/solution.java
@@ -1,1 +1,32 @@
+class Solution {
+    
+    public static String doSort(String input){
+        char[] arr=input.toCharArray();
+        Arrays.sort(arr);
+        
+        return new String(arr);
 
+    }
+    public ArrayList<ArrayList<String>> anagrams(String[] arr) {
+        // code here
+         ArrayList<ArrayList<String>> ans=new ArrayList<>();
+        
+        Map<String,ArrayList<String>> map=new TreeMap<>();
+        for(String st:arr){
+            String sorted=doSort(st);
+            
+            if(!map.containsKey(sorted)){
+                map.put(sorted,new ArrayList<>());
+            }
+            map.get(sorted).add(st);
+            
+        }
+        
+        for(Map.Entry<String,ArrayList<String>> mpEle : map.entrySet()){
+            ans.add(mpEle.getValue());
+            
+        }
+        
+        return ans;
+    }
+}
