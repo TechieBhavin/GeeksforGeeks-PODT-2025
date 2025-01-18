af54bc4efce169fcf57265bb5a586467f597f6d2
Patel Bhavin <154894074+TechieBhavin@users.noreply.github.com>
2025-01-18 23:08:52 +0530

    Update solution.java

--- PODT-of-JANUARY/Day18-Reverse a linked list/solution.java
+++ PODT-of-JANUARY/Day18-Reverse a linked list/solution.java
@@ -1,1 +1,29 @@
 
+// function Template for Java
+
+/* linked list node class:
+
+class Node {
+    int data;
+    Node next;
+    Node(int value) {
+        this.value = value;
+    }
+}
+
+*/
+
+class Solution {
+    Node reverseList(Node head) {
+        // code here
+        Node prev = null;
+        Node cur = head;
+        while(cur != null){
+            Node next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+}
